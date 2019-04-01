873a0ad96c553467b9d18a2c19b637ed32eff2ae
HiLanXiao <1256925921@qq.com>
2019-04-01 12:28:33 +0800

    Third Update

--- pages/shouye/shouye.js
+++ pages/shouye/shouye.js
@@ -1,7 +1,20 @@
+const app = getApp();
 Page({
+
+  data: {
+  
+  },
+
+
   bindItemTap: function () {
     wx.navigateTo({
       url: '../tiezi/tiezi'
+    })
+  },
+
+  tz: function () {
+    wx.navigateTo({
+      url: '../dayi/dayi'
     })
   }
   
