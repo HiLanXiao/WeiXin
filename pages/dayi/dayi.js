a9cc31ccad282c57071fd164a340abc71eceba1c
HiLanXiao <1256925921@qq.com>
2019-04-04 11:09:24 +0800

    fourth update

--- pages/dayi/dayi.js
+++ pages/dayi/dayi.js
@@ -1,1 +1,36 @@
-Page({})
+Page({
+
+  onShow: function () {
+    const session_id = wx.getStorageSync('session_id');
+    if (!session_id) {
+      wx.showToast({
+        title: '正在登录',
+        icon: 'succes',
+        duration: 2000,
+        mask: true
+      })
+
+      wx.login({
+        success(res) {
+          if (res.code) {
+            wx.request({
+              url: 'https://test.com/onLogin',
+              data: {
+                code: res.code
+              },
+              success(res) {
+                wx.setStorageSync('session_id', res.session_id)
+              }
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
+        }
+      })
+
+    }
+  },
+
+
+
+})
--- pages/wode/wode.js
+++ pages/wode/wode.js
@@ -3,52 +3,38 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+
+  
+
+
+  onShow: function () {
+    const session_id = wx.getStorageSync('session_id');
+    if (!session_id) {
+      wx.showToast({
+        title: '正在登录',
+        icon: 'succes',
+        duration: 2000,
+        mask: true
       })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+
+      wx.login({
+        success(res) {
+          if (res.code) {
+            wx.request({
+              url: 'https://test.com/onLogin',
+              data: {
+                code: res.code
+              },
+              success(res) {
+                wx.setStorageSync('session_id', res.session_id)
+              }
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
         }
       })
+
     }
   },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
